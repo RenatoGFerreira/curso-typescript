fc4802ba312196fc6f385cf4315371fb214a11a0
RenatoFerreira <renatoyferreira@gmail.com>
2023-09-12 17:04:35 -0300

    aula03

--- aula-ts-02/src/index.ts
+++ aula-ts-02/src/index.ts
@@ -2,6 +2,5 @@ function soma(num1:number, num2:number){
     return num1 + num2;
 }
 
-const result = soma(1, 2)
-
+const result = soma(4, 2)
 console.log(result)
---
+++ aula-ts-03/src/index.ts
@@ -0,0 +1,14 @@
+// inferência de tipos
+function soma(num1:number, num2:number){
+    return num1 + num2;
+}
+
+//any e void
+
+const num: number = 42;
+const text: string = "hello";
+const valid: boolean = true;
+
+
+const result = soma(4, 2)
+console.log(result) 
