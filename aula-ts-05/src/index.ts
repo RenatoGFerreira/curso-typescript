74a6c9db26a8f0647b421d936e9394edbd99d702
RenatoFerreira <renatoyferreira@gmail.com>
2023-09-14 20:29:29 -0300

    aula-ts-05

---
+++ aula-ts-05/src/index.ts
@@ -0,0 +1,11 @@
+const numbers: number[] = [1,2,3,4,5,6]
+const booleanos: boolean[]= [true, true, false, true]
+const strings: string[] = ["one", "two", "tres", "quatorze"]
+
+const numbers2: Array<number> = [1,2,3,4,5,6]
+const booleanos2: Array<boolean>= [true, true, false, true]
+const strings2: Array<String> = ["one", "two", "tres", "quatorze"]
+
+
+const mix: (string | number)[] = [1, "two", 3, "four"]
+const mix2: Array<string | number> = [1, "two", 3, "four"]
